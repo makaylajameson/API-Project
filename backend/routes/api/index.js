eb1d720812ddcdf041c20db5c879729b4806e3c2
Makayla Jameson <makayla.jameson01@gmail.com>
2023-06-08 17:02:06 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,14 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+const { restoreUser } = require('../../utils/auth.js');
+
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+
+router.use(restoreUser);
 
 module.exports = router;
