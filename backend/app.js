a603b2b2c4f1a1549c6900cd914404df9d1517f5
Makayla Jameson <makayla.jameson01@gmail.com>
2023-06-08 13:09:20 -0500

    Add an API router & Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+// Sequelize Error-Handler
+const { ValidationError } = require('sequelize');
 
 // Create a variable called isProduction that will be true if the environment
 // is in production or not by checking the environment key in the configuration file
@@ -50,5 +52,40 @@ if (!isProduction) {
 
   const routes = require('./routes'); // is this in the right spot?
   app.use(routes); // Connect all the routes
+
+  // Catch unhandled requests and forward to error handler.
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+  // Process sequelize errors
+  app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+   if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+   }
+      next(err);
+  });
+
+  // Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+     errors: err.errors,
+     stack: isProduction ? null : err.stack
+    });
+  });
 
   module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api'); // is this in the right spot?
+router.use('/api', apiRouter);
+
 // In this test route, you are setting a cookie on the response with the name
 // of XSRF-TOKEN to the value of the req.csrfToken method's return. Then, you
 // are sending the text, Hello World! as the response's body.
