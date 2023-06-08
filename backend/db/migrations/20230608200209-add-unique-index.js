370876f0ac6287aaab4c22d63ee9a80d5998d074
Makayla Jameson <makayla.jameson01@gmail.com>
2023-06-08 15:46:36 -0500

    Add Users table migration and seeds

---
+++ backend/db/migrations/20230608200209-add-unique-index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addIndex('Users', {
+      fields: ['username', 'email'],
+      unique: true,
+      name: 'idx_unique_user_un_e'
+  })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeIndex('Users', 'idx_unique_user_un_e')
+  }
+};
