d9e8fae22e5ebd8755263cbe9c90439d9afc336d
Makayla Jameson <makayla.jameson01@gmail.com>
2023-06-08 12:04:28 -0500

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/config/database.js
@@ -0,0 +1,30 @@
+// backend/config/database.js
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  }
+};
+
+// This will allow you to load the database configuration environment
+// variables from the .env file into the config/index.js, as well as
+// define the global schema for the project.
---
+++ backend/routes/index.js
@@ -0,0 +1,25 @@
+// Create an index.js file in the routes folder. In this file,
+// create an Express router, create a test route, and export the router
+// at the bottom of the file.
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+// In this test route, you are setting a cookie on the response with the name
+// of XSRF-TOKEN to the value of the req.csrfToken method's return. Then, you
+// are sending the text, Hello World! as the response's body.
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// }); // comment this out because I passed the test
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+module.exports = router;
