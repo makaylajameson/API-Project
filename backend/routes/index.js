7f3431495c8177479fe54c97d2717571913a2004
Makayla Jameson <makayla.jameson01@gmail.com>
2023-06-30 08:03:16 -0500

    phase 0

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,37 @@ const router = express.Router();
 const apiRouter = require('./api'); // is this in the right spot?
 router.use('/api', apiRouter);
 
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 // In this test route, you are setting a cookie on the response with the name
 // of XSRF-TOKEN to the value of the req.csrfToken method's return. Then, you
 // are sending the text, Hello World! as the response's body.
